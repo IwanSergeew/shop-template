5a1570385bb5c1287cc7faffb823df50ab84c3b3
Ivan Sergeev <49801117+IwanSergeew@users.noreply.github.com>
2022-07-28 14:44:41 +0300

    update

--- assets/js/products-filter.js
+++ assets/js/products-filter.js
@@ -1,25 +1,33 @@
 class ProductsFilter {
     constructor({
         selector,
-        ajax
+        ajax,
+        perPage = 20,
+        filters = {
+            searchBar: true
+        }
     }) {
         if(!selector) return console.error('Invalid selector provided');
         this.el = typeof(selector) == 'string' ? document.querySelector(selector) : selector;
-        this.category = '';
         if(ajax) {
             if(typeof(ajax) == 'string') this.ajaxUrl = ajax;
             else {
                 this.ajaxUrl = ajax.url || null;
                 if(ajax.method && ajax.method != 'POST' && ajax.method != 'GET') return console.error(`Invalid ajax method given.`);
-                this.ajaxMethod = ajax.method || 'POST';
-                this.dataSrc = ajax.dataSrc || '';
+                this.ajaxMethod = (ajax.method && (ajax.method == 'POST' || ajax.method == 'GET')) ? ajax.method : 'POST';
             }
         }
+        this.page = 0;
+        this.perPage = perPage;
+        this.orderby = 0;
+        this.order_direction = 'ASC';
+        this.searchText = '';
+        this.category = '';
+        this.filters = filters;
 
         Array.from(this.el.querySelectorAll('ul.filter-menu li[data-category]')).forEach(e => {
             e.addEventListener('click', this.categoryClickEvent);
         });
-        this.el.querySelector('.menu-btn').addEventListener('click', this.mobileFilterMenuToggle);
 
         this.createFilterOptions();
 
@@ -27,46 +35,16 @@ class ProductsFilter {
     }
 
     getProductsData = async () => {
-        console.log('here');
         try {
-            let response;
-            if(!this.ajaxMethod || this.ajaxMethod == 'POST') {
-                const fd = new FormData();
-                fd.append('page', this.page);
-                fd.append('limit', this.perPage);
-                fd.append('orderby', this.orderby);
-                fd.append('order_direction', this.order_direction);
-                fd.append('search_text', this.searchText);
-
-                response = await fetch(this.ajaxUrl, {
-                    method: 'POST',
-                    body: fd
-                });
-            }
-            else {
-                response = await fetch(this.ajaxUrl + '?' + new URLSearchParams({
-                    page: this.page,
-                    limit: this.perPage,
-                    orderby: this.orderby,
-                    order_direction: this.order_direction,
-                    searchText: this.searchText,
-                }), {
-                    method: 'GET'
-                });
-            }
+            const response = await fetch(this.ajaxUrl, { method: this.ajaxMethod });
             if(!response) throw 'No data recieved from request.';
-            const json = await response.json();
-            const data = (this.dataSrc && this.dataSrc.length > 0) ? json[0][this.dataSrc] : json[0];
-            this.totalItems = data.total_rows;
-            if(!this.totalItems) this.noDataFound();
+            this.data = await response.json();
+            if(!this.data?.length) this.noDataFound();
             else {
-                this.appendProducts(data.data);
+                this.appendProducts();
                 this.addPagination();
             }
-            if(this.initComplete && typeof(this.initComplete) == 'function') this.initComplete({
-                data: data.data,
-                total_rows: data.total_rows
-            });
+            if(this.initComplete && typeof(this.initComplete) == 'function') this.initComplete();
         }
         catch(err) {
             if(this.error && typeof(this.error) == 'function') this.error(err);
@@ -75,13 +53,36 @@ class ProductsFilter {
     }
 
     noDataFound = () => {
-
+        const container = this.el.querySelector('.products-container');
+        container.innerHTML = 'No products found.';
     }
 
     createFilterOptions = () => {
         const container = document.createElement('div');
         container.classList = 'filter-options';
+        const menuBtn = document.createElement('div');
+        menuBtn.classList = 'menu-btn';
+        menuBtn.innerHTML = '=';
+        menuBtn.addEventListener('click', this.mobileFilterMenuToggle);
+        container.append(menuBtn);
+        if(this.filters.searchBar) {
+            const searchBarEl = document.createElement('input');
+            searchBarEl.type = 'text';
+            searchBarEl.classList = 'search-filter';
+            searchBarEl.placeholder = 'Search...';
+            searchBarEl.addEventListener('keyup', (e) => {
+                if(this.searchTimeout) clearTimeout(this.searchTimeout);
+                this.searchTimeout = setTimeout(this.searchFilterChange, 200, e);
+            });
+            container.append(searchBarEl);
+        }
         this.el.prepend(container);
+    }
+
+    searchFilterChange = (e) => {
+        this.searchTimeout = null;
+        this.searchText = e.target.value;
+        this.appendProducts();
     }
 
     mobileFilterMenuToggle = (e) => {
@@ -93,9 +94,61 @@ class ProductsFilter {
         e.currentTarget.classList.add('active');
         const category = e.currentTarget.dataset.category;
         this.category = category;
+        this.appendProducts();
     }
 
     appendProducts = () => {
+        this.filterData();
+        if(!this.filteredData.length) {
+            this.noDataFound();
+            return;
+        }
+        const container = this.el.querySelector('.products-container');
+        container.innerHTML = '';
+        let a, imageContainer, img, name, rating, y, hy, b;
+        for(let i = (this.page * this.perPage); i < ((this.page + 1) * this.perPage); i++) {
+            if(i >= this.filteredData.length) break;
+            a = document.createElement('a');
+            a.classList = 'product-wrapper';
+            a.href = `products?id=${this.filteredData[i]['id']}`;
+            imageContainer = document.createElement('div');
+            imageContainer.classList = 'product-image';
+            img = document.createElement('img');
+            img.src = this.filteredData[i]['image'];
+            img.alt = 'Product Image';
+            name = document.createElement('span');
+            name.classList = 'product-name';
+            name.innerHTML = this.filteredData[i]['name'];
+            rating = document.createElement('span');
+            rating.classList = 'product-rating';
+            [y, hy, b] = this.getRateStars(this.filteredData[i]['rating']);
+            rating.innerHTML = '<span class="yellow">' + '★'.repeat(y) + '</span>' + (hy ? '<span class="half-yellow">★</span>' : '') + '★'.repeat(b);
 
+            imageContainer.append(img);
+            a.append(imageContainer);
+            a.append(name);
+            a.append(rating);
+            container.append(a);
+        }
+    }
+
+    addPagination = () => {
+
+    }
+
+    getRateStars(n){
+        const nInt = parseInt(n);
+        if(nInt == n) return [nInt, 0, 5-n];
+        const decimal = parseInt((n % 1) * 100);
+        if(decimal < 25) return [nInt-1, 0, 5-(nInt - 1)];
+        if(decimal > 75) return [nInt, 0, 5-nInt];
+        return [nInt-1, 1, 5-nInt];
+    }
+
+    filterData = () => {
+        this.filteredData = this.data.filter(v => 
+            (!this.category || v.category == this.category) &&
+            (this.searchText.length == 0 || v.name.toLowerCase().includes(this.searchText.toLowerCase()))
+        );
     }
 }
--- data/data-products.php
+++ data/data-products.php
@@ -1,7 +1,9 @@
 <?php
-    $qColumns = [ "id", "name", "image", "category", "rating" ];
-    $qFrom = "products";
-    $qJoin = "";
-    $qWhere = "";
-    
-    include 'products-data-func.php';
+    $config = include_once 'config.php';
+    $conn = new mysqli($config['host'], $config['name'], $config['pass'], $config['db']);
+    if(!$conn) exit;
+
+    $result = $conn->query("SELECT * FROM products");
+
+    echo json_encode($result->fetch_all(MYSQLI_ASSOC));
+    exit;
